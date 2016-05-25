207e445de6eb0182fb22fcc1d1852cae430b368a
nas <nas@5f41e2b4-0802-45f1-ab2b-32947f5ba958>
2016-05-25 14:47:33 +0000

    Initial commit

---
+++ ErrorBag.php
@@ -0,0 +1,38 @@
+<?php
+namespace TrunkSoftware\Component\Errors;
+
+class ErrorBag {
+	private $errors = array();
+
+	public function addError( ErrorInterface $error )
+	{
+		$this->errors[] = $error;
+
+		return $this;
+	}
+
+	public function getErrors() {
+		return $this->errors;
+	}
+
+	public function getCount() {
+		return count( $this->errors );
+	}
+
+	public function getFirst() {
+		return isset($this->errors[0]) ? $this->errors[0] : null;
+	}
+
+	/**
+	 * Transform all ErrorInterface to array and return
+	 * @return array
+	 */
+	public function toArray() {
+		$data = array();
+		foreach( $this->errors as $error ) {
+			$data[] = $error->toArray();
+		}
+
+		return $data;
+	}
+}
