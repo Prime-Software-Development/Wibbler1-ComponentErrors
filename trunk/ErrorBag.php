67a91bda084f4c60cdd91d02f9582e4bf35d0bdc
nas <nas@5f41e2b4-0802-45f1-ab2b-32947f5ba958>
2016-11-22 16:55:51 +0000

    Added Countable interface to ErrorBag\nAdded clear method to ErrorBag

--- trunk/ErrorBag.php
+++ trunk/ErrorBag.php
@@ -1,7 +1,11 @@
 <?php
 namespace TrunkSoftware\Component\Errors;
 
-class ErrorBag {
+class ErrorBag implements \Countable {
+
+	/**
+	 * @var Error[] $errors
+	 */
 	private $errors = array();
 
 	public function addError( ErrorInterface $error )
@@ -11,16 +15,38 @@ class ErrorBag {
 		return $this;
 	}
 
+	/**
+	 * @return Error[]
+	 */
 	public function getErrors() {
 		return $this->errors;
 	}
 
+	/**
+	 * @return int
+	 */
 	public function getCount() {
-		return count( $this->errors );
+		return $this->count();
 	}
 
+	/**
+	 * @return null|Error
+	 */
 	public function getFirst() {
 		return isset($this->errors[0]) ? $this->errors[0] : null;
+	}
+
+	/**
+	 * Clear all errors and return them
+	 *
+	 * @return Error[]
+	 */
+	public function clear()
+	{
+		$errors = $this->errors;
+		$this->errors = array();
+
+		return $errors;
 	}
 
 	/**
@@ -43,5 +69,19 @@ class ErrorBag {
 	public function toJSON()
 	{
 		return json_encode( $this->toArray() );
+	}
+
+	/**
+	 * Count elements of an object
+	 * @link  http://php.net/manual/en/countable.count.php
+	 * @return int The custom count as an integer.
+	 * </p>
+	 * <p>
+	 * The return value is cast to an integer.
+	 * @since 5.1.0
+	 */
+	public function count() {
+
+		return count($this->errors);
 	}
 }
